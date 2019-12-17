a62e4af31219201004da701c97ea2ac781ce3bd6
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-17 20:14:15 +0200

    getting started with M ui

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Header} from '../Layouts'
+
+function App() {
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
+};
+export default App
---
+++ src/Layouts/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AppBar, Toolbar} from '@material-ui/core/';
+
+export default function Header() {
+    return (
+        <React.Fragment>
+            <AppBar>
+                <Toolbar>
+                    
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    )
+}
