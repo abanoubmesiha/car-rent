12739f1467ebe4d9e858202963b8a1976cbdc610
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-17 20:21:39 +0200

    nav 1 2 3 Buttons

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import {AppBar, Toolbar} from '@material-ui/core/';
+import {AppBar, Toolbar, Button } from '@material-ui/core/';
 
 export default function Header() {
     return (
         <React.Fragment>
             <AppBar>
                 <Toolbar>
-                    
+                    <Button color="inherit">Nav 1</Button>
+                    <Button color="inherit">Nav 2</Button>
+                    <Button color="inherit">Nav 3</Button>
                 </Toolbar>
             </AppBar>
         </React.Fragment>
